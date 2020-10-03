9271a9991bd97c4d1c8af1f46aa3340980599cf8
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-03 14:21:43 -0700

    add post backend

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,24 +16,24 @@ router.post(
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array });
+			return res.status(400).json({ errors: errors.array() });
 		}
 
 		try {
 			const user = await User.findById(req.user.id).select("-password");
 
-			const newPost = {
+			const newPost = new Post({
 				text: req.body.text,
 				name: user.name,
 				avatar: user.avatar,
 				user: req.user.id
-			};
+			});
 
 			const post = await newPost.save();
 
 			res.json(post);
-		} catch (error) {
-			console.error(error.message);
+		} catch (err) {
+			console.error(err.message);
 			res.status(500).send("Server Error");
 		}
 	}
