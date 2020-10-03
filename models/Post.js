fb92f31fa1ec1e02128f44d2a48d743196e1d7bc
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-03 14:21:05 -0700

    add post backend

--- models/Post.js
+++ models/Post.js
@@ -13,5 +13,39 @@ const PostSchema = new Schema({
 	name: {
 		type: String
 	},
-	avatar: {}
+	avatar: {
+		type: String
+	},
+	likes: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: "users"
+			}
+		}
+	],
+	comments: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: "users"
+			},
+			text: {
+				type: String,
+				required: true
+			},
+			name: {
+				type: String
+			},
+			avatar: {
+				type: String
+			},
+			date: {
+				type: Date,
+				default: Date.now
+			}
+		}
+	]
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);
