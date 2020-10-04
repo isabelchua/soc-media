ed2d7b473a3928e0ec9eb47ca1d0ac83b5e2f657
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-03 20:49:22 -0700

    login register frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-	return <>hi</>;
+	return (
+		<Router>
+			<Navbar />
+			<Route excat path="/" component={Landing} />
+			<section className="container">
+				<Switch>
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/login" component={Login} />
+				</Switch>
+			</section>
+		</Router>
+	);
 }
 
 export default App;
