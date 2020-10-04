1cad990c8173118c63fe5f6557fff429acfb1b97
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-04 01:15:51 -0700

    added prof

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 const PrivateRoute = ({
@@ -18,6 +19,10 @@ const PrivateRoute = ({
 		}
 	/>
 );
+
+PrivateRoute.propTypes = {
+	auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
 	auth: state.auth
