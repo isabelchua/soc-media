99980f7065f30e0a3460f0859600d2daca53410b
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-06 10:22:05 -0700

    delete post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import { getPost } from "../../actions/post";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
 	useEffect(() => {
@@ -18,6 +20,16 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
 				Back To posts
 			</Link>
 			<PostItem post={post} showActions={false} />
+			<CommentForm postId={post._id} />
+			<div className="comments">
+				{post.comments.map(comment => (
+					<CommentItem
+						key={comment._id}
+						comment={comment}
+						postId={post._id}
+					/>
+				))}
+			</div>
 		</Fragment>
 	);
 };
