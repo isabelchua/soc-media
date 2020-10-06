08401d79037d8d29964cabd5d17f1b0e621e4065
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-05 23:27:03 -0700

    add post error

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token);
@@ -65,6 +66,7 @@ function App() {
 							path="/add-education"
 							component={AddEducation}
 						/>
+						<PrivateRoute exact path="/posts" component={Posts} />
 					</Switch>
 				</section>
 			</Router>
---
+++ client/src/actions/post.js
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { response } from "express";
+import { setAlert } from "./alert";
+import {
+	DELETE_POST,
+	GET_POSTS,
+	POST_ERROR,
+	UPDATE_LIKES,
+	ADD_POST
+} from "./types";
+
+export const getPosts = () => async dispatch => {
+	try {
+		const res = await axios.get("/api/posts");
+
+		dispatch({
+			type: GET_POSTS,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status
+			}
+		});
+	}
+};
+
+// add like
+
+export const addLike = id => async dispatch => {
+	try {
+		const res = await axios.put(`/api/posts/like/${id}`);
+
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data }
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status
+			}
+		});
+	}
+};
+
+// remove like
+
+export const removeLike = id => async dispatch => {
+	try {
+		const res = await axios.put(`/api/posts/unlike/${id}`);
+
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data }
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status
+			}
+		});
+	}
+};
+
+// Delete post
+export const deletePost = id => async dispatch => {
+	try {
+		console.log("delete");
+		await axios.delete(`/api/posts/${id}`);
+
+		dispatch({
+			type: DELETE_POST,
+			payload: id
+		});
+
+		dispatch(setAlert("Post Removed", "success"));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+	try {
+		const res = await axios.post(`/api/posts`, formData, config);
+
+		dispatch({
+			type: ADD_POST,
+			payload: res.data
+		});
+
+		dispatch(setAlert("Post Created", "success"));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
